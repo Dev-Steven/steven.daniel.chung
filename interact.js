0ff01f3c283a061f593accb133848ae793d8f55b
Steven <steven.chung96@yahoo.com>
2019-03-27 22:27:14 -0700

    First commit

---
+++ interact.js
@@ -0,0 +1,14 @@
+function goIG() {
+    url = "https://www.instagram.com/stevendotdaniel/?hl=en";
+    window.open(url);
+}
+
+function goFB() {
+    url = "https://www.facebook.com/steven.chung.9041";
+    window.open(url);
+}
+
+function goLI() {
+    url = "https://www.linkedin.com/in/steven-chung-4174b9179/";
+    window.open(url);
+}
